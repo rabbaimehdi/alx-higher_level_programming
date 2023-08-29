387ca319a8187ad43853f7d64516a6163f8a4dcf
Mehdi <mehdi.rabbai@gmail.com>
2023-08-28 18:18:57 -0700

    update 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(f"{my_list[i]}", end="")
             sum += 1
-        except IndexError:
+        except:
             break
     print()
     return(sum)
