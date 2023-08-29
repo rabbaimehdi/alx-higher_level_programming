4528a55900775e832b2b6794d60a16ad35e7dd3e
Mehdi <mehdi.rabbai@gmail.com>
2023-08-29 16:39:52 -0700

    add tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a square """
+
+
+class Square:
+    """a square"""
+
+    def __init__(self, size):
+        """Initializing the square
+        Args: size - the size of the square defined
+        """
+        self.__size = size
