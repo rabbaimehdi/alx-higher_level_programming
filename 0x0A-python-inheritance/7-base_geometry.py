ddcbf0cf461d70de8fd99decfe6232e9df4c0479
Mehdi <mehdi.rabbai@gmail.com>
2023-09-11 18:32:26 -0700

    correct pycodestyle for 7 & 9

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
     def area(self):
         """method not implemented"""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """validates a value as an int
         """
