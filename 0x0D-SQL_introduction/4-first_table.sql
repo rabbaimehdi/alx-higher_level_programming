56ea7b9ea4c42d3b9c235b16be8c0e183c7f175d
Mehdi <mehdi.rabbai@gmail.com>
2023-10-17 18:21:44 -0700

    add tasks from 0 to 14

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- create a table called first_table in the current database
+-- first_table description:
+--             id INT
+--             name VARCHAR(256)
+-- If the table first_table already exists, the script should not fail
+
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+
