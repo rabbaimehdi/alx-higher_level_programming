c62ce24fbadfae7a4c3f8f51bc77fae06264a52d
Mehdi <mehdi.rabbai@gmail.com>
2023-11-13 15:52:12 -0800

    correct style for tasks 1, 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-if ( argv.length > 2){
-   console.log("Argument found");
+if (argv.length > 2) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
-else {
-    console.log("No argument");
-};
