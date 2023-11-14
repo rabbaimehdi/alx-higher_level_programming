ec6747ed8c92de96d3f19618e703ad6fcbe9c319
Mehdi <mehdi.rabbai@gmail.com>
2023-11-13 16:51:09 -0800

    add task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let index = 0;
+
+while (index < array.length) {
+  console.log(array[index]);
+  index++;
+}
