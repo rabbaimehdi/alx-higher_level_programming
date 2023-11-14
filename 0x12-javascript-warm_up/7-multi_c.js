ee5392ba3384d19e8589bee24e1c72544847c5fa
Mehdi <mehdi.rabbai@gmail.com>
2023-11-13 17:14:26 -0800

    correction task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@ const argv = process.argv;
 const integer = parseInt(argv[2]);
 
 if (isNaN(integer) || integer === undefined) {
-	  console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	for (let index = 0; index < integer; index++) {
-		    console.log('C is fun');
-	}
+  for (let index = 0; index < integer; index++) {
+    console.log('C is fun');
+  }
 }
