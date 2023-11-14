f20fdb481959def33fed0b27f13a5471c7fabb78
Mehdi <mehdi.rabbai@gmail.com>
2023-11-13 17:36:25 -0800

    correction task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ const argv = process.argv;
 const a = parseInt(argv[2]);
 const b = parseInt(argv[3]);
 
-function add(a, b) {
-    console.log(a + b);
+function add (a, b) {
+  console.log(a + b);
 }
 add(a, b);
