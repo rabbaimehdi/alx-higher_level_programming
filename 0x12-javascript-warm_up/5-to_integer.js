16b64903f9b4d5e09d48613a0e04a2f738c61512
Mehdi <mehdi.rabbai@gmail.com>
2023-11-13 16:35:07 -0800

    correction task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const argv = process.argv;
-const integer = parseInt(argv[2])
+const integer = parseInt(argv[2]);
 
 if (isNaN(integer) || integer === undefined) {
   console.log('Not a number');
