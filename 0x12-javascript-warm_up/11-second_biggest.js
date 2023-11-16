e07bf4a3de4ec462a3e2ed51a66740156ed497f4
Mehdi <mehdi.rabbai@gmail.com>
2023-11-15 16:55:58 -0800

    add correction for task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const argv = process.argv;
 if (argv.length <= 3) {
   console.log(0);
 } else {
-  const list = argv.slice(2);
-  list.sort();
-  console.log(list[1]);
+  const list = argv.slice(2).map(Number);
+  list.sort((a, b)=>(a - b));
+  console.log(list[list.length-2]);
 }
