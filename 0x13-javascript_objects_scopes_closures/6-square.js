b37d0dbc6d594c9dddc85996cc0045604c7f6b7e
Mehdi <mehdi.rabbai@gmail.com>
2023-11-15 16:05:03 -0800

    add task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Square_ = require('./5-square.js');
+
+class Square extends Square_ {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        let printChar = '';
+        for (let j = 0; j < this.size; j++) {
+          printChar += c;
+        }
+        console.log(printChar);
+      }
+    }
+  }
+}
+module.exports = Square;
