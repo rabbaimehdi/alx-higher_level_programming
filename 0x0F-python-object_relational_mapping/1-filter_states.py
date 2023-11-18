cb8b78a03f805a46d627a1550f8f2b39d84acdd1
Mehdi <mehdi.rabbai@gmail.com>
2023-11-18 06:03:51 -0800

    add correction for task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ def main():
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name like\
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
                 'N%' ORDER BY id ASC")
     query_row = cur.fetchall()
     for row in query_row:
