58d41c8a27e79cddd857cd498448c6b77d89ed7f
Mehdi <mehdi.rabbai@gmail.com>
2023-11-18 06:19:23 -0800

    Add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that lists all states = user input
+from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' \
+                 ORDER BY id ASC".format(sys.argv[4]))
+    query_row = cur.fetchall()
+    for row in query_row:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
