94bd81ea1c09f74e5024113da5bb7ceeeaf26fda
Mehdi <mehdi.rabbai@gmail.com>
2023-12-01 16:02:09 -0800

    correction for task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,7 +10,7 @@ from sys import argv
 
 def main():
     try:
-        with request.urlopen(argv[1]) as response:
+        with request.urlopen(request.Request(argv[1])) as response:
             print(request.read().decode("ascii"))
     except error.HTTPError as e:
         print(f"Error code: {e.code}")
