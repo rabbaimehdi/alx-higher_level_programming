fb409fcd476ebb61b1448fdd9996a750d67beb11
Mehdi <mehdi.rabbai@gmail.com>
2023-12-02 12:52:53 -0800

    Correction for Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,10 +5,9 @@ https://alx-intranet.hbtn.io/status
 """
 import requests
 
-
 def main():
     body = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response")
+    print("Body response:")
     print(f"\t- type: {type(body.text)}")
     print(f"\t- content: {body.text}")
 
