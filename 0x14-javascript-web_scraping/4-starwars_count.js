14cd88b4d3c7bb8df3f94c0d2410f958f332b7f0
mehdi <mehdi.rabbai@gmail.com>
2024-01-03 15:55:02 -0800

    add correction for task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
 const characterId = '18';
 const url = process.argv[2];
+let counter = 0;
 
 request(url, (error, response, body) => {
   if (error) {
     console.log(error, response.statusCode);
   } else {
     body = JSON.parse(body);
-    let counter = 0;
     body.results.forEach(film => {
       film.characters.forEach(character => {
         if (character.includes(characterId)) {
