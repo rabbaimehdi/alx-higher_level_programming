946f5f66861f2629d27f107098a16111980de293
mehdi <mehdi.rabbai@gmail.com>
2024-01-03 16:35:23 -0800

    add correction for task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,19 +8,18 @@ const url = `https://swapi.dev/api/films/${movieId}/`;
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  const characters = data.characters;
-  for (const character of characters) {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
+  } else {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    characters.forEach(character => {
+      request(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
+        }
+      });
     });
   }
 });
