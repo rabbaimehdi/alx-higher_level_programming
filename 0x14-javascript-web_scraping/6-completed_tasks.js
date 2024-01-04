51409af584b14e31cad9f672539c2a48c585385f
mehdi <mehdi.rabbai@gmail.com>
2024-01-03 16:18:27 -0800

    add correction for task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,18 +2,7 @@
 
 const request = require('request');
 const url = process.argv[2];
-const dict = {
-  1: 0,
-  2: 0,
-  3: 0,
-  4: 0,
-  5: 0,
-  6: 0,
-  7: 0,
-  8: 0,
-  9: 0,
-  10: 0
-};
+const dict = {};
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
@@ -21,7 +10,11 @@ request(url, (error, response, body) => {
     body = JSON.parse(body);
     body.forEach(todo => {
       if (todo.completed) {
-        dict[todo.userId] += 1;
+        if (!dict[todo.userId]) {
+          dict[todo.userId] = 1;
+        } else {
+          dict[todo.userId] += 1;
+        }
       }
     });
     console.log(dict);
